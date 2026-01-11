4b3276fb4c91ff02ee4d7e22f546fd822eadecf0
GreenGeezy <Agcomsol@gmail.com>
2026-01-11 13:57:50 -0600

    Update MainActivity.java

--- android/app/src/main/java/com/mastergrowbot/app/MainActivity.java
+++ android/app/src/main/java/com/mastergrowbot/app/MainActivity.java
@@ -2,14 +2,11 @@ package com.mastergrowbot.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.revenuecat.purchases.capacitor.PurchasesPlugin; // Import RevenueCat
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Manual Registration to fix "Plugin not implemented" error
-        registerPlugin(PurchasesPlugin.class);
+        // No manual registration needed for Capacitor 6+
     }
 }
